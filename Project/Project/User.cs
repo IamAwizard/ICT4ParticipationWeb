36c667915e5a43c97ff46f852e2e93fbef861288
Maurice <m.harsveld@student.fontys.nl>
2016-01-07 10:57:13 +0100

    user klasse

--- Project/Project/User.cs
+++ Project/Project/User.cs
@@ -7,5 +7,15 @@ namespace Project
 {
     public class User
     {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string Location { get; set; }
+        public int Phonenumber { get; set; }
+        public string license { get; set; }
+        public string hascar { get; set; }
+        public string dateofwriteout { get; set; }
+
+        public User(string name, string adress,)
     }
 }
