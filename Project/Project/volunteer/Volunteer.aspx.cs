a7246415d747d5674c4acd427d76a2ca3490a641
dundy <dundy_pura_7@live.com>
2016-01-07 11:28:54 +0100

    volunteer

--- Project/Project/volunteer/Volunteer.aspx.cs
+++ Project/Project/volunteer/Volunteer.aspx.cs
@@ -7,11 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace Project.volunteer
 {
-	public partial class Volunteer : System.Web.UI.MasterPage
-	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
+    public partial class Volunteer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
 
-		}
-	}
+        }
+    }
 }
